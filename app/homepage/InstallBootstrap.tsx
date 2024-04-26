901b52efbc1a06c0b86cc6f27904119ac65a8ddf
unknown <aman.100100@gmail.com>
2024-04-26 13:32:20 +0530

    add bootstrap

--- app/homepage/InstallBootstrap.tsx
+++ app/homepage/InstallBootstrap.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function InstallBootstrap(){
     useEffect(()=>{
-        import("bootstrap/dist/js/bootstrap.bundle.js");
+        import("bootstrap");
     },[])
     return(
         <></>
